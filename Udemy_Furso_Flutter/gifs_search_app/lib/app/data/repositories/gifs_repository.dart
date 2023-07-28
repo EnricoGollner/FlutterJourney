eeb5705d842d829ffa57081cc1f6d743edcdd11b
Enrico Gollner <enricogollner.tech@gmail.com>
2023-07-28 05:00:23 -0300

    V1.0 - Gif Searcher

--- Udemy_Furso_Flutter/gifs_search_app/lib/app/data/repositories/gifs_repository.dart
+++ Udemy_Furso_Flutter/gifs_search_app/lib/app/data/repositories/gifs_repository.dart
@@ -21,11 +21,11 @@ class GifsRepository {
     if (search == null) {
       response = await httpClient.get(
           url:
-              "https://api.giphy.com/v1/gifs/trending?api_key=M5aofUBX7sU5uRE78ObQHC5AxojGorMg&limit=20&offset=0&rating=g&bundle=messaging_non_clips");
+              "https://api.giphy.com/v1/gifs/trending?api_key=/* YOUR API KEY */&limit=20&offset=0&rating=g&bundle=messaging_non_clips");
     } else {
       response = await httpClient.get(
           url:
-              "https://api.giphy.com/v1/gifs/search?api_key=M5aofUBX7sU5uRE78ObQHC5AxojGorMg&q=$search&limit=19&offset=$offset&rating=g&lang=en&bundle=messaging_non_clips");
+              "https://api.giphy.com/v1/gifs/search?api_key=/* YOUR API KEY */&q=$search&limit=19&offset=$offset&rating=g&lang=en&bundle=messaging_non_clips");
     }
 
     final Map<String, dynamic> jsonDecoded = json.decode(response.body);
