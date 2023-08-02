bd96a72728b485ab83a4bc41bfdfb65fc8778ab4
Enrico Gollner <enricogollner.tech@gmail.com>
2023-08-02 10:56:14 -0300

    Chat app iniciado com Firebase Flutter app

---
+++ Udemy_Curso_Flutter/chat_online_app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:chat_online_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'app/main_app.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MainApp());
+}
