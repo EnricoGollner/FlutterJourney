96b2238406283c9a84efd1f39426a40bbc17b8a5
Enrico Gollner <enricogollner.tech@gmail.com>
2023-08-19 12:15:48 -0300

    counter com streams

---
+++ flutter_stream_counter/lib/main.dart
@@ -0,0 +1,85 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  final StreamController<int> _streamController =
+      StreamController<int>(); // sink limitado apenas a int
+
+// Lifecycle de encerramento do widget
+  @override
+  void dispose() {
+    _streamController.close(); // fechamos aqui a StreamController - IMPORTANTE
+    super.dispose();
+  }
+
+  void _incrementCounter() {
+    _counter++;
+    _streamController.sink.add(_counter);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            StreamBuilder<int>(
+                stream: _streamController.stream,
+                initialData: 0,
+                builder: (context, snapshot) {
+                  return Text(
+                    "${snapshot.data}",
+                    style: const TextStyle(fontSize: 20),
+                  );
+                }),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
