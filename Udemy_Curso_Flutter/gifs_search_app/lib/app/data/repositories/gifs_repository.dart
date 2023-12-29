6dafff32179ab6f1a1483416f3d55e5533db4891
EnricoGollner <enricogollner.tech@gmail.com>
2023-12-29 16:04:34 -0300

    Corrigindo Git ignore, estruturando pastas e dotenv

--- Udemy_Curso_Flutter/gifs_search_app/lib/app/data/repositories/gifs_repository.dart
+++ Udemy_Curso_Flutter/gifs_search_app/lib/app/data/repositories/gifs_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:gifs_search_app/app/data/http/http_client.dart';
 import 'package:gifs_search_app/app/data/models/gif_model.dart';
 import 'package:http/http.dart' as http;
@@ -18,20 +19,18 @@ class GifsRepository {
 
     http.Response response;
 
+    await dotenv.load(fileName: '.env');
+    String apiKey = dotenv.env["API_KEY"]!;
+
     if (search == null) {
-      response = await httpClient.get(
-          url:
-              "https://api.giphy.com/v1/gifs/trending?api_key=M5aofUBX7sU5uRE78ObQHC5AxojGorMg&limit=20&offset=0&rating=g&bundle=messaging_non_clips");
+      response = await httpClient.get(url: "https://api.giphy.com/v1/gifs/trending?api_key=M5aofUBX7sU5uRE78ObQHC5AxojGorMg&limit=20&offset=0&rating=g&bundle=messaging_non_clips");
     } else {
-      response = await httpClient.get(
-          url:
-              "https://api.giphy.com/v1/gifs/search?api_key=M5aofUBX7sU5uRE78ObQHC5AxojGorMg&q=$search&limit=19&offset=$offset&rating=g&lang=en&bundle=messaging_non_clips");
+      response = await httpClient.get(url: "https://api.giphy.com/v1/gifs/search?api_key=$apiKey=$search&limit=19&offset=$offset&rating=g&lang=en&bundle=messaging_non_clips");
     }
 
     final Map<String, dynamic> jsonDecoded = json.decode(response.body);
     final List<dynamic> data = jsonDecoded["data"];
-    final List<GifModel> gifList =
-        data.map((e) => GifModel.fromMap(e)).toList();
+    final List<GifModel> gifList = data.map((e) => GifModel.fromMap(e)).toList();
 
     return gifList;
   }
